3bd4f924a5d246e2babc575d7088cf2520e852b6
Nenad Laptop <nenadsombor.rs@gmail.com>
2023-04-06 19:26:56 +0200

    Room host can change settings

--- control_music/api/views.py
+++ control_music/api/views.py
@@ -97,14 +97,13 @@ class LeaveRoom(APIView):
         if 'room_code' in self.request.session:
             host_id = self.request.session.session_key
             room_results = Room.objects.filter(host=host_id)
-            if len(room_results) > 0:
+            if room_results.exists():
                 room = room_results[0]
                 room.delete()
         return Response({"Message": "Success"}, status=status.HTTP_200_OK)
 
 
 class UpdateRoom(APIView):
-
     serializer_class = UpdateRoomSerializer
 
     def patch(self, request, format=None):
@@ -118,19 +117,17 @@ class UpdateRoom(APIView):
             code = serializer.data.get('code')
 
             queryset = Room.objects.filter(code=code)
-
             if not queryset.exists():
-                return Response({"msg": "Room not found"}, status=status.HTTP_404_NOT_FOUND)
+                return Response({'msg': 'Room not found.'}, status=status.HTTP_404_NOT_FOUND)
 
             room = queryset[0]
             user_id = self.request.session.session_key
             if room.host != user_id:
-                return Response({"msg": "You are not the host"}, status=status.HTTP_404_NOT_FOUND)
-            
+                return Response({'msg': 'You are not the host of this room.'}, status=status.HTTP_403_FORBIDDEN)
+
             room.guest_can_pause = guest_can_pause
             room.votes_to_skip = votes_to_skip
-            room.save(update_fields=["guest_can_pause","votes_to_skip"])
+            room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+            return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
 
-            return Response(RoomSerializer(room).data,status=status.HTTP_200_OK)
-
-        return Response({"Bad Request": "Invalid Point"}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'Bad Request': "Invalid Data..."}, status=status.HTTP_400_BAD_REQUEST)
